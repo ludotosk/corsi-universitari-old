f2611f9cc957c1c2a9582e7121fdab8e77bf806d
LudoTosk <toscanoludovico@gmail.com>
2020-11-18 19:12:47 +0100

    creata tabella semplice

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
+  <v-container>
+    <v-simple-table fixed-header height="100%">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Nome corso</th>
+            <th class="text-left">Tipo corso</th>
+            <th class="text-letf">Università</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in corsi" :key="item.titoloTxt">
+            <td>
+              <a :href="item.hrefTxt">{{ item.titoloTxt }}</a>
+            </td>
+            <td>{{ item.tipoLaurea }}</td>
+            <td>{{ item.uni }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>
-import corsi from "../public/corsi.json"
+import corsiFile from "../public/corsi.json";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      corsi: corsiFile,
+    };
+  },
 };
 </script>
