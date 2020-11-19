6c4c39090dd13e7dd95def551763ece576d0deca
LudoTosk <toscanoludovico@gmail.com>
2020-11-19 19:13:37 +0100

    aggiunta bocconi

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <v-table :data="corsi">
-    <thead slot="head">
-      <th>Nome corso</th>
-      <th>Tipo Laurea</th>
-      <th>Università</th>
-    </thead>
-      <tbody slot="body" slot-scope="{displayData}">
-        <tr v-for="row in displayData" :key="row.titoloTxt">
-          <td><a :href="row.hrefTxt">{{ row.titoloTxt }}</a></td>
+  <div>
+    <label>Filtra in base al nome:</label>
+    <input class="form-control" v-model="filters.nomeCorso.value" />
+    <v-table :data="corsi" :filters="filters">
+      <thead slot="head">
+        <th>Nome corso</th>
+        <th>Tipo Laurea</th>
+        <th>Università</th>
+      </thead>
+      <tbody slot="body" slot-scope="{ displayData }">
+        <tr v-for="row in displayData" :key="row.guid">
+          <td>
+            <a :href="row.hrefTxt">{{ row.nomeCorso }}</a>
+          </td>
           <td>{{ row.tipoLaurea }}</td>
           <td>{{ row.uni }}</td>
         </tr>
-    </tbody>
-  </v-table>
+      </tbody>
+    </v-table>
+  </div>
 </template>
 
 <script>
@@ -23,7 +29,18 @@ export default {
   data() {
     return {
       corsi,
+      filters: {
+        nomeCorso: { value: "", keys: ["nomeCorso"] },
+      },
     };
   },
 };
 </script>
+
+<style>
+table,
+th,
+td {
+  border: 1px solid black;
+}
+</style>
--- src/scrapers.js
+++ src/scrapers.js
@@ -7,7 +7,7 @@ async function scrapeUnimi(url, browser) {
     await page.goto(url);
 
     var hrefTxt = '';
-    var titoloTxt = '';
+    var nomeCorso = '';
     var tipoLaurea = '';
 
     var next = [];
@@ -22,13 +22,13 @@ async function scrapeUnimi(url, browser) {
         if (el != undefined) {
             const titolo = await el.getProperty('textContent');
             const link = await el.getProperty('href');
-            titoloTxt = await titolo.jsonValue();
+            nomeCorso = await titolo.jsonValue();
             hrefTxt = await link.jsonValue();
         } else {
             const [el2] = await page.$x('/html/body/div/div/div/section/div/div[2]/div/div[2]/div/div/div/div/div[2]/div[' + i + ']/div/div/div/div/div/div/div/div[2]/div[3]/a');
             const titolo = await el2.getProperty('textContent');
             const link = await el2.getProperty('href');
-            titoloTxt = await titolo.jsonValue();
+            nomeCorso = await titolo.jsonValue();
             hrefTxt = await link.jsonValue();
         }
 
@@ -44,7 +44,7 @@ async function scrapeUnimi(url, browser) {
         }
 
         tipoLaurea = tipoLaurea.trim()
-        arrayCorsi.push({titoloTxt,hrefTxt,tipoLaurea,uni});
+        arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
 
         i += 1;
         [next] = await page.$x('/html/body/div/div/div/section/div/div[2]/div/div[2]/div/div/div/div/div[2]/div[' + i + ']');
@@ -70,7 +70,7 @@ async function scrapeUnimib(url, browser) {
         do {
             const [el] = await page.$x('/html/body/div[6]/div[3]/section/div/section/div/div/div[2]/div[4]/div/div/fieldset[1]/div/div/div/div/div/div/div[' + j + ']/div[2]/h3[' + i + ']/a/div');
             const titolo = await el.getProperty('textContent');
-            var titoloTxt = await titolo.jsonValue();
+            var nomeCorso = await titolo.jsonValue();
 
             const [el1] = await page.$x('/html/body/div[6]/div[3]/section/div/section/div/div/div[2]/div[4]/div/div/fieldset[1]/div/div/div/div/div/div/div[' + j + ']/div[2]/div[' + i + ']/div/div[4]/a');
             var hrefTxt = '';
@@ -85,10 +85,21 @@ async function scrapeUnimib(url, browser) {
 
             const [el3] = await page.$x('/html/body/div[6]/div[3]/section/div/section/div/div/div[2]/div[4]/div/div/fieldset[1]/div/div/div/div/div/div/div[' + j + ']/div[2]/div[' + i + ']/div/div[2]/span');
             const tipoCorso = await el3.getProperty('textContent');
-            const tipoLaurea = await tipoCorso.jsonValue();
+            var tipoLaurea = await tipoCorso.jsonValue();
 
-            titoloTxt = titoloTxt.trim();
-            arrayCorsi.push({titoloTxt,hrefTxt,tipoLaurea,uni});
+            nomeCorso = nomeCorso.trim();
+            if (tipoLaurea == 'Laurea triennale (DM 270)'){
+                tipoLaurea = 'Laurea triennale';
+            } else {
+                if(tipoLaurea == 'Corso di Laurea Magistrale'){
+                    tipoLaurea = 'Laurea magistrale';
+                } else {
+                    if(tipoLaurea == 'Laurea Magistrale Ciclo Unico 5 anni' || tipoLaurea == 'Laurea Magistrale Ciclo Unico 6 anni'){
+                        tipoLaurea = 'Laurea magistrale a ciclo unico';
+                    }
+                }
+            }
+            arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
 
             i += 1;
             [next] = await page.$x('/html/body/div[6]/div[3]/section/div/section/div/div/div[2]/div[4]/div/div/fieldset[1]/div/div/div/div/div/div/div[' + j + ']/div[2]/h3[' + i + ']/a/div');
@@ -118,10 +129,10 @@ async function scrapePolimi(url, browser, tipoLaurea) {
         const [el] = await page.$x('/html/body/div/div/div/div[3]/div/div/ul[2]/li[' + i + ']/a');
         const titolo = await el.getProperty('textContent');
         const link = await el.getProperty('href');
-        const titoloTxt = await titolo.jsonValue();
+        const nomeCorso = await titolo.jsonValue();
         const hrefTxt = await link.jsonValue();
 
-        arrayCorsi.push({titoloTxt,hrefTxt,tipoLaurea,uni});
+        arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
 
         i += 1;
         [next] = await page.$x('/html/body/div/div/div/div[3]/div/div/ul[2]/li[' + i + ']/a');
@@ -130,23 +141,76 @@ async function scrapePolimi(url, browser, tipoLaurea) {
     return (arrayCorsi);
 }
 
+async function scrapeBocconi(url, browser) {
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const uni = 'bocconi';
+
+    var arrayCorsi = [];
+
+    var next = [];
+
+    var i = 1;
+    var j = 1;
+
+    do {
+        do {
+            const [el] = await page.$x('/html/body/div[3]/div[2]/article/div[1]/div[1]/div/ul/li[' + j + ']/ul/li[' + i + ']/a');
+            const titolo = await el.getProperty('textContent');
+            const link = await el.getProperty('href');
+            const nomeCorso = await titolo.jsonValue();
+            const hrefTxt = await link.jsonValue();
+
+            tipoLaurea = 'Laurea triennale';
+            arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
+
+            i += 1;
+            [next] = await page.$x('/html/body/div[3]/div[2]/article/div[1]/div[1]/div/ul/li[' + j + ']/ul/li[' + i + ']/a');
+        } while (next != undefined);
+        j += 1;
+        i = 1;
+        [next] = await page.$x('/html/body/div[3]/div[2]/article/div[1]/div[1]/div/ul/li[' + j + ']/ul');
+    } while (next != undefined)
+
+    i = 1;
+
+    do {
+        const [el] = await page.$x('/html/body/div[3]/div[2]/article/div[1]/div[2]/div/ul/li[' + i + ']/a');
+        const titolo = await el.getProperty('textContent');
+        const link = await el.getProperty('href');
+        const nomeCorso = await titolo.jsonValue();
+        const hrefTxt = await link.jsonValue();
+
+        tipoLaurea = 'Laurea magistrale';
+        arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
+
+        i += 1;
+        [next] = await page.$x('/html/body/div[3]/div[2]/article/div[1]/div[2]/div/ul/li[' + i + ']/a');
+    } while (next != undefined);
+
+    return (arrayCorsi);
+}
 
 
 async function launchScrape() {
     const browser = await puppeteer.launch();
 
-    const corsiUnimiTriennale = await scrapeUnimi('https://www.unimi.it/it/corsi/corsi-di-laurea-triennali-e-magistrali-ciclo-unico', browser);
-    const corsiUnimiMagistrale = await scrapeUnimi('https://www.unimi.it/it/corsi/corsi-di-laurea-magistrale', browser);
-    const corsiUnimib = await scrapeUnimib('https://www.unimib.it/didattica/corsi-studio-iscrizioni', browser);
-    const corsiPolimiTriennale = await scrapePolimi('https://www.polimi.it/corsi/corsi-di-laurea/', browser, 'Laurea triennale');
-    const corsiPolimiMagistrale = await scrapePolimi('https://www.polimi.it/corsi/corsi-di-laurea-magistrale/', browser, 'Laurea magistrale');
+    //const corsiUnimiTriennale = await scrapeUnimi('https://www.unimi.it/it/corsi/corsi-di-laurea-triennali-e-magistrali-ciclo-unico', browser);
+    //const corsiUnimiMagistrale = await scrapeUnimi('https://www.unimi.it/it/corsi/corsi-di-laurea-magistrale', browser);
+    //const corsiUnimib = await scrapeUnimib('https://www.unimib.it/didattica/corsi-studio-iscrizioni', browser);
+    //const corsiPolimiTriennale = await scrapePolimi('https://www.polimi.it/corsi/corsi-di-laurea/', browser, 'Laurea triennale');
+    //const corsiPolimiMagistrale = await scrapePolimi('https://www.polimi.it/corsi/corsi-di-laurea-magistrale/', browser, 'Laurea magistrale');
+    const corsiBocconi = await scrapeBocconi('https://www.unibocconi.it/wps/wcm/connect/bocconi/sitopubblico_it/albero+di+navigazione/home/corsi+di+studio/', browser);
 
     var corsi = [];
-    Array.prototype.push.apply(corsi,corsiUnimiTriennale);
-    Array.prototype.push.apply(corsi,corsiUnimiMagistrale);
-    Array.prototype.push.apply(corsi,corsiUnimib);
-    Array.prototype.push.apply(corsi,corsiPolimiTriennale);
-    Array.prototype.push.apply(corsi,corsiPolimiMagistrale); 
+    //Array.prototype.push.apply(corsi,corsiUnimiTriennale);
+    //Array.prototype.push.apply(corsi,corsiUnimiMagistrale);
+    //Array.prototype.push.apply(corsi,corsiUnimib);
+    //Array.prototype.push.apply(corsi,corsiPolimiTriennale);
+    //Array.prototype.push.apply(corsi,corsiPolimiMagistrale);
+    Array.prototype.push.apply(corsi,corsiBocconi);
     
    fs.writeFile('../public/corsi.json', JSON.stringify(corsi), function (err) {
         if (err) return console.log(err);
