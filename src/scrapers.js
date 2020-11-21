05e1836d2949ae6a1de5f491e9571fa0f14b1c82
LudoTosk <toscanoludovico@gmail.com>
2020-11-22 00:16:53 +0100

    aggiunta unibg

--- src/scrapers.js
+++ src/scrapers.js
@@ -44,7 +44,7 @@ async function scrapeUnimi(url, browser) {
         }
 
         tipoLaurea = tipoLaurea.trim()
-        arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
+        arrayCorsi.push({ nomeCorso, hrefTxt, tipoLaurea, uni });
 
         i += 1;
         [next] = await page.$x('/html/body/div/div/div/section/div/div[2]/div/div[2]/div/div/div/div/div[2]/div[' + i + ']');
@@ -88,18 +88,18 @@ async function scrapeUnimib(url, browser) {
             var tipoLaurea = await tipoCorso.jsonValue();
 
             nomeCorso = nomeCorso.trim();
-            if (tipoLaurea == 'Laurea triennale (DM 270)'){
+            if (tipoLaurea == 'Laurea triennale (DM 270)') {
                 tipoLaurea = 'Laurea triennale';
             } else {
-                if(tipoLaurea == 'Corso di Laurea Magistrale'){
+                if (tipoLaurea == 'Corso di Laurea Magistrale') {
                     tipoLaurea = 'Laurea magistrale';
                 } else {
-                    if(tipoLaurea == 'Laurea Magistrale Ciclo Unico 5 anni' || tipoLaurea == 'Laurea Magistrale Ciclo Unico 6 anni'){
+                    if (tipoLaurea == 'Laurea Magistrale Ciclo Unico 5 anni' || tipoLaurea == 'Laurea Magistrale Ciclo Unico 6 anni') {
                         tipoLaurea = 'Laurea magistrale a ciclo unico';
                     }
                 }
             }
-            arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
+            arrayCorsi.push({ nomeCorso, hrefTxt, tipoLaurea, uni });
 
             i += 1;
             [next] = await page.$x('/html/body/div[6]/div[3]/section/div/section/div/div/div[2]/div[4]/div/div/fieldset[1]/div/div/div/div/div/div/div[' + j + ']/div[2]/h3[' + i + ']/a/div');
@@ -132,7 +132,7 @@ async function scrapePolimi(url, browser, tipoLaurea) {
         const nomeCorso = await titolo.jsonValue();
         const hrefTxt = await link.jsonValue();
 
-        arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
+        arrayCorsi.push({ nomeCorso, hrefTxt, tipoLaurea, uni });
 
         i += 1;
         [next] = await page.$x('/html/body/div/div/div/div[3]/div/div/ul[2]/li[' + i + ']/a');
@@ -164,7 +164,7 @@ async function scrapeBocconi(url, browser) {
             const hrefTxt = await link.jsonValue();
 
             tipoLaurea = 'Laurea triennale';
-            arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
+            arrayCorsi.push({ nomeCorso, hrefTxt, tipoLaurea, uni });
 
             i += 1;
             [next] = await page.$x('/html/body/div[3]/div[2]/article/div[1]/div[1]/div/ul/li[' + j + ']/ul/li[' + i + ']/a');
@@ -184,7 +184,7 @@ async function scrapeBocconi(url, browser) {
         const hrefTxt = await link.jsonValue();
 
         tipoLaurea = 'Laurea magistrale';
-        arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
+        arrayCorsi.push({ nomeCorso, hrefTxt, tipoLaurea, uni });
 
         i += 1;
         [next] = await page.$x('/html/body/div[3]/div[2]/article/div[1]/div[2]/div/ul/li[' + i + ']/a');
@@ -196,11 +196,53 @@ async function scrapeBocconi(url, browser) {
     const nomeCorso = await titolo.jsonValue();
     const hrefTxt = await link.jsonValue();
 
-    arrayCorsi.push({nomeCorso,hrefTxt,tipoLaurea,uni});
+    arrayCorsi.push({ nomeCorso, hrefTxt, tipoLaurea, uni });
 
     return (arrayCorsi);
 }
 
+async function scrapeUnibgCorso(url, browser, arrayCorsi){
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const [el] = await page.$x('/html/body/div[2]/div/div/section/h1');
+    const titolo = await el.getProperty('textContent');
+    const nomeCorso = await titolo.jsonValue();
+    console.log(nomeCorso);
+
+    const [el1] = await page.$x('/html/body/div[2]/div/div/section/div/div[1]/div/section/div/div[3]/div[2]/div[2]/div');
+    const laurea = await el1.getProperty('textContent');
+    const tipoLaurea = await laurea.jsonValue();
+
+    const uni = 'unibg';
+    const hrefTxt = uni;
+
+    arrayCorsi.push({ nomeCorso, hrefTxt, tipoLaurea, uni });
+
+}
+
+async function scrapeUnibg(url, browser) {
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    var arrayCorsi = [];
+
+    const list = await page.evaluateHandle(() => {
+        return Array.from(document.getElementById('contenitore-principale').getElementsByTagName('a')).map(a => a.href);
+    });
+    var lista = await list.jsonValue();
+
+    for (var i = 0, max = lista.length; i < max; i++) {
+        if (lista[i].length == 37) {
+            //console.log(lista[i]);
+            await scrapeUnibgCorso(lista[i], browser, arrayCorsi);
+        }
+    }
+
+    return (arrayCorsi);
+}
 
 async function launchScrape() {
     const browser = await puppeteer.launch();
@@ -210,7 +252,9 @@ async function launchScrape() {
     //const corsiUnimib = await scrapeUnimib('https://www.unimib.it/didattica/corsi-studio-iscrizioni', browser);
     //const corsiPolimiTriennale = await scrapePolimi('https://www.polimi.it/corsi/corsi-di-laurea/', browser, 'Laurea triennale');
     //const corsiPolimiMagistrale = await scrapePolimi('https://www.polimi.it/corsi/corsi-di-laurea-magistrale/', browser, 'Laurea magistrale');
-    const corsiBocconi = await scrapeBocconi('https://www.unibocconi.it/wps/wcm/connect/bocconi/sitopubblico_it/albero+di+navigazione/home/corsi+di+studio/', browser);
+    //const corsiBocconi = await scrapeBocconi('https://www.unibocconi.it/wps/wcm/connect/bocconi/sitopubblico_it/albero+di+navigazione/home/corsi+di+studio/', browser);
+    const corsiUnibgTriennale = await scrapeUnibg('https://www.unibg.it/studia-noi/corsi/lauree-triennali-e-ciclo-unico', browser);
+    const corsiUnibgMagistrale = await scrapeUnibg('https://www.unibg.it/studia-noi/corsi/lauree-magistrali', browser);
 
     var corsi = [];
     //Array.prototype.push.apply(corsi,corsiUnimiTriennale);
@@ -218,9 +262,11 @@ async function launchScrape() {
     //Array.prototype.push.apply(corsi,corsiUnimib);
     //Array.prototype.push.apply(corsi,corsiPolimiTriennale);
     //Array.prototype.push.apply(corsi,corsiPolimiMagistrale);
-    Array.prototype.push.apply(corsi,corsiBocconi);
-    
-   fs.writeFile('../public/corsi.json', JSON.stringify(corsi), function (err) {
+    //Array.prototype.push.apply(corsi,corsiBocconi);
+    Array.prototype.push.apply(corsi, corsiUnibgTriennale);
+    Array.prototype.push.apply(corsi, corsiUnibgMagistrale);
+
+    fs.writeFile('../public/corsi.json', JSON.stringify(corsi), function (err) {
         if (err) return console.log(err);
         console.log('corsi > corsi.json');
     });
