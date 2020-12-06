2064525c316996144da605c686194d124a79762b
ludotosk <toscanoludovico@gmail.com>
2020-12-06 10:27:38 +0100

    first commit

---
+++ src/scaper2.js
@@ -0,0 +1,258 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+var corsi = [];
+
+//creare singoli json per aree accademiche
+//creare json unico per ricerca generica
+//modificare il select id del area accademica clickare cerca prendere i dati
+
+async function ScrapeArea(page) {
+    const records = await page.$x('/html/body/div[3]/div/div[2]/div[2]/div[2]/div/table/tbody/tr');
+    var uni = '';
+    var i;
+    for (i = 1; i < records.length + 1; i++) {
+        var [el] = await page.$x('/html/body/div[3]/div/div[2]/div[2]/div[2]/div/table/tbody/tr[' + i + ']/td[2]/strong');
+        if (el == undefined) {
+            [el] = await page.$x('/html/body/div[3]/div/div[2]/div[2]/div[2]/div/table/tbody/tr[' + i + ']/th/h3');
+            //console.log('uni');
+            const universita = await el.getProperty('textContent');
+            uni = await universita.jsonValue();
+        } else {
+            //console.log('corso');
+            //console.log(i);
+            const titolo = await el.getProperty('textContent');
+            var nomeCorso = await titolo.jsonValue();
+
+            [el] = await page.$x('/html/body/div[3]/div/div[2]/div[2]/div[2]/div/table/tbody/tr[' + i + ']/td[2]/a[3]');
+            if (el == undefined) {
+                var hrefTxt = 'fix';
+            } else {
+                const link = await el.getProperty('href');
+                var hrefTxt = await link.jsonValue();
+
+            }
+
+            [el] = await page.$x('/html/body/div[3]/div/div[2]/div[2]/div[2]/div/table/tbody/tr[' + i + ']/td[3]/span');
+            const classe = await el.getProperty('textContent');
+            const classeTxt = await classe.jsonValue();
+            if (classeTxt.slice(0, 2) == 'L-') {
+                var tipoLaurea = 'Laurea Triennale';
+            } else {
+                var tipoLaurea = 'Laurea Magistrale';
+            }
+
+            corsi.push({ nomeCorso, hrefTxt, tipoLaurea, uni });
+        }
+    }
+    await page.evaluate(() => {
+        var tabella = document.querySelector('#risultati > div.skin > div > table');
+        tabella.remove();
+    });
+}
+
+async function laucnhScrape() {
+    const inizio = Date.now();
+
+    console.log('Start');
+
+    const browser = await puppeteer.launch({ headless: true, });
+    const page = await browser.newPage();
+
+    console.log('Browser aperto');
+
+    await page.setRequestInterception(true);
+
+    //if the page makes a  request to a resource type of image or stylesheet then abort that request
+    page.on('request', request => {
+        if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() == 'font' || request.resourceType() == 'media')
+            request.abort();
+        else
+            request.continue();
+    });
+
+    await page.goto('https://www.universitaly.it/index.php/cercacorsi/universita');
+
+    console.log('Pagina aperta');
+
+    //primo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '01';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio prima lista')
+    await ScrapeArea(page);
+    console.log('fine prima lista');
+
+    //secondo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '02';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inzio seconda lista')
+    await ScrapeArea(page);
+    console.log('fine seconda lista');
+
+    //terzo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '03';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio terza lista')
+    await ScrapeArea(page);
+    console.log('fine terza lista');
+
+    //quarto valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '04';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inzio quarta lista')
+    await ScrapeArea(page);
+    console.log('fine quarta lista');
+
+    //quinto valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '05';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio quinta lista')
+    await ScrapeArea(page);
+    console.log('fine quinta lista');
+
+    //sesto valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '06';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inzio sesta lista')
+    await ScrapeArea(page);
+    console.log('fine sesta lista');
+
+    //settimo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '07';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio settima lista')
+    await ScrapeArea(page);
+    console.log('fine settima lista');
+
+    //ottavo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '08';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inzio ottava lista')
+    await ScrapeArea(page);
+    console.log('fine ottava lista');
+
+    //nono valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '09';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio nona lista')
+    await ScrapeArea(page);
+    console.log('fine nona lista');
+
+    //decimo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '10';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inzio decima lista')
+    await ScrapeArea(page);
+    console.log('fine decima lista');
+
+    //unidicesimo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '11';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio undicesima lista')
+    await ScrapeArea(page);
+    console.log('fine undicesima lista');
+
+    //dodicesimo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '12';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio dodicesima lista')
+    await ScrapeArea(page);
+    console.log('fine dodicesima lista');
+
+    //tredicesimo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '13';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio tredicesima lista')
+    await ScrapeArea(page);
+    console.log('fine tredicesima lista');
+
+    //quattordicesimo valore
+    await page.evaluate(() => {
+        document.querySelector('#areacun').value = '14';
+    });
+
+    await page.click('#avvia_ricerca');
+    await page.waitForSelector('#risultati > div.skin > div > table');
+
+    console.log('inizio quattordicesima lista')
+    await ScrapeArea(page);
+    console.log('fine quattordicesima lista');
+
+    page.close();
+    browser.close();
+
+    fs.writeFile('../public/corsi.json', JSON.stringify(corsi), function (err) {
+        if (err) return console.log(err);
+        console.log('corsi > corsi.json');
+        const fine = (Date.now() - inizio) / 1000;
+        console.log('tempo in secondi', fine);
+        process.exit();
+    });
+}
+
+laucnhScrape();
