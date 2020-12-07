3441875de96ed2ccad43c97ebe0dae8e22754ee6
ludotosk <toscanoludovico@gmail.com>
2020-12-07 21:46:46 +0100

    aggiunta pagina privacy

---
+++ src/cookie-policy.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="container">
+    <br />
+    <h1>Informativa sui cookie</h1>
+    <h2>Cosa sono i cookie?</h2>
+    <p>
+      I cookie sono piccoli file di testo che i siti visitati dagli utenti
+      inviano ai loro terminali, ove vengono memorizzati per essere poi
+      ritrasmessi agli stessi siti alla visita successiva. I cookie delle c.d.
+      "terze parti" vengono, invece, impostati da un sito web diverso da quello
+      che l´utente sta visitando. Questo perché su ogni sito possono essere
+      presenti elementi (immagini, mappe, suoni, specifici link a pagine web di
+      altri domini, ecc.) che risiedono su server diversi da quello del sito
+      visitato.
+    </p>
+    <h2>A cosa servono i cookie?</h2>
+    <p>
+      I cookie sono usati per differenti finalità: esecuzione di autenticazioni
+      informatiche, monitoraggio di sessioni, memorizzazione di informazioni su
+      specifiche configurazioni riguardanti gli utenti che accedono al server,
+      memorizzazione delle preferenze, ecc.
+    </p>
+    <h2>Cosa sono i cookie "tecnici"?</h2>
+    <p>
+      Sono i cookie che servono a effettuare la navigazione o a fornire un
+      servizio richiesto dall´utente. Non vengono utilizzati per scopi ulteriori
+      e sono normalmente installati direttamente dal titolare del sito web.
+    </p>
+    <p>
+      Senza il ricorso a tali cookie, alcune operazioni non potrebbero essere
+      compiute o sarebbero più complesse e/o meno sicure, come ad esempio le
+      attività di home banking (visualizzazione dell´estratto conto, bonifici,
+      pagamento di bollette, ecc.), per le quali i cookie, che consentono di
+      effettuare e mantenere l´identificazione dell´utente nell´ambito della
+      sessione, risultano indispensabili.
+    </p>
+    <h2>
+      cookie tecnici che non richiedono consenso all'utente di terze parti
+    </h2>
+    <h5>Servizio statistico di Google Analytics (IP anonimizzato)</h5>
+    <p><strong>Nome del cookie:</strong>_ga</p>
+    <p><strong>Durata:</strong>2 anni</p>
+    <p>
+      <strong>Descrizione:</strong>analisi anonima delle statistiche di
+      navigazione
+    </p>
+    <br />
+    <p><strong>Nome del cookie:</strong>_gat</p>
+    <p><strong>Durata:</strong>a fine sessione</p>
+    <p>
+      <strong>Descrizione:</strong>analisi anonima delle statistiche di
+      navigazione
+    </p>
+    <br />
+    <p><strong>Nome del cookie:</strong>_gaid</p>
+    <p><strong>Durata:</strong>24 ore</p>
+    <p>
+      <strong>Descrizione:</strong>analisi anonima delle statistiche di
+      navigazione
+    </p>
+    <h5>
+      Per maggiori dettagli, invitiamo a leggere le informative di riferimento:
+    </h5>
+    <p>Privacy e protezione dei dati</p>
+    <a
+      href="https://support.google.com/analytics/topic/2919631?hl=it&ref_topic=1008008"
+      >https://support.google.com/analytics/topic/2919631?hl=it&ref_topic=1008008</a
+    >
+    <p>Salvaguardia dei dati</p>
+    <a href="https://support.google.com/analytics/answer/6004245"
+      >https://support.google.com/analytics/answer/6004245</a
+    >
+    <p>Informativa Cookie di Google per Google Analytics</p>
+    <a
+      href="https://developers.google.com/analytics/devguides/collection/analyticsjs/cookie-usage"
+      >https://developers.google.com/analytics/devguides/collection/analyticsjs/cookie-usage</a
+    >
+    <p>
+      Componente aggiuntivo del browser per la disattivazione di Google
+      Analytics
+    </p>
+    <a href="https://tools.google.com/dlpage/gaoptout?hl=it"
+      >https://tools.google.com/dlpage/gaoptout?hl=it</a
+    >
+    <br />
+    <p>
+      <strong>Come disabilitare i cookie dalle impostazioni del browser</strong>
+    </p>
+    <p>
+      Ricordiamo che non è obbligatorio acquisire il consenso alla operatività
+      dei soli cookie tecnici o di terze parti o analitici assimilati ai cookie
+      tecnici. La loro disattivazione e/o il diniego alla loro operatività potrà
+      comportare l’impossibilità di una corretta navigazione sul sito. In ogni
+      caso, i cookie devono poter essere gestiti dall’utente e, di seguito, si
+      indicano le modalità sulla base del browser utilizzato:
+    </p>
+    <p>
+      Mozilla Firefox
+      <a href="https://support.mozilla.org/it/kb/Bloccare%20i%20cookie"
+        >link alle istruzioni</a
+      >
+    </p>
+    <p>
+      Google Chrome
+      <a href="https://support.google.com/chrome/answer/95647?hl=it"
+        >link alle istruzioni</a
+      >
+    </p>
+    <p>
+      Microsoft Internet Explorer
+      <a
+        href="http://windows.microsoft.com/it-it/windows-vista/block-or-allow-cookies"
+        >link alle istruzioni</a
+      >
+    </p>
+    <p>
+      Microsoft Edge
+      <a
+        href="https://privacy.microsoft.com/it-it/windows-10-microsoft-edge-and-privacy"
+        >link alle istruzioni</a
+      >
+    </p>
+    <p>
+      Opera
+      <a href="http://help.opera.com/Windows/10.00/it/cookies.html"
+        >link alle istruzioni</a
+      >
+    </p>
+    <p>
+      Safari
+      <a href="https://support.apple.com/kb/PH21411?locale=it_IT"
+        >link alle istruzioni</a
+      >
+    </p>
+    <br />
+    <p>
+      Per browser diversi da quelli in elenco consultare la relativa guida per
+      individuare le modalità di gestione dei cookie.
+    </p>
+    <br />
+    <p><strong>Titolare del sito web</strong></p>
+    <p>Ludovico Toscano</p>
+    <p>
+      Email:<a href="mailto:toscanoludovico@gmail.com"
+        >toscanoludovico@gmail.com</a
+      >
+    </p>
+  </div>
+</template>
