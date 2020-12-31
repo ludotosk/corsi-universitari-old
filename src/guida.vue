b65c4e07313bfc0f260bf815a8aaed010635a5f9
ludotosk <toscanoludovico@gmail.com>
2020-12-31 18:00:52 +0100

    aggiunta guida youtube e email

---
+++ src/guida.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-md">
+      <br>
+      <h1 class="text-secondary">Guida all'utilizzo del sito</h1>
+      <hr>
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe
+        class="embed-responsive-item"
+        src="https://www.youtube.com/embed/ZOE7AzL7ncs"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</template>
