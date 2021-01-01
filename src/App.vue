b31657b50e8f5b56318333ca430cff764523a749
ludotosk <toscanoludovico@gmail.com>
2021-01-01 11:35:42 +0100

    fixato footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <router-view name="header"></router-view>
-    <router-view></router-view>
+    <router-view class="min-vh-100"></router-view>
     <router-view name="footer"></router-view>
   </div>
 </template>
--- src/guida.vue
+++ src/guida.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-md vh-100">
+  <div class="container-md">
     <br />
     <h1 class="text-secondary">Guida all'utilizzo del sito</h1>
     <h2 class="text-secondary">
