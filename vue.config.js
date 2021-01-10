911f98b092f7c3223d6a6e903fbf909fd8518f1c
ludotosk <toscanoludovico@gmail.com>
2021-01-10 11:46:27 +0100

    cambiate impostazione prerenderer

--- vue.config.js
+++ vue.config.js
@@ -91,7 +91,7 @@ const productionPlugins = [
       // so we create a snapshot of the page after the
       // `data-view` attribute exists in the DOM.
       renderAfterElementExists: '[data-view]',
-      headless: false,
+      headless: true,
       maxConcurrentRoutes: 4
     }),
   }),
