eb88c0bab22cfb9d6e43c0f033ed7d3e4657dc46
ludotosk <toscanoludovico@gmail.com>
2021-03-02 12:05:43 +0100

    fix generali e aggiunta economia

---
+++ src/area/corsi-di-laurea-economia.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="container-md">
+    <br />
+    <h1 class="text-secondary">Corsi di laurea {{ uni }}</h1>
+    <br />
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <label class="input-group-text" id="basic-addon3" for="basic-url"
+          >Filtra in base al nome:</label
+        >
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        id="basic-url"
+        aria-describedby="basic-addon3"
+        v-model="filters.n.value"
+      />
+    </div>
+    <p>
+      <strong>Attenzione!</strong> per eseguire la ricerca serve il nome del
+      corso corretto. Es. <strong>biotecnologie</strong> si trova sotto
+      "biotecnologie" "biotecnologia" "biotechnology" quindi per avere il
+      risultato corretto inserire <strong>"biotec"</strong>. Cioè la parte
+      comune a tutti i nomi. Il resto della <strong>guida</strong> è sotto la
+      tabella.
+    </p>
+    <v-table
+      :data="corsi"
+      :filters="filters"
+      :pageSize="15"
+      @totalPagesChanged="totalPages = $event"
+      :currentPage.sync="currentPage"
+      class="table table-striped table-hover table-sm table-bordered"
+    >
+      <thead slot="head" class="thead-dark">
+        <th>Corso di laurea</th>
+        <v-th sortKey="t" defaultSort="asc">Livello</v-th>
+        <v-th sortKey="a" defaultSort="asc">Test</v-th>
+        <v-th sortKey="u" defaultSort="asc">Università</v-th>
+      </thead>
+      <tbody slot="body" slot-scope="{ displayData }" data-view>
+        <tr v-for="row in displayData" :key="row.guid">
+          <td>
+           <a :href="row.h" target="_blank" rel="noopener" class="text-danger">{{ row.n }}</a>
+          </td>
+          <td>Corso di Laurea {{ row.t }}</td>
+          <td>{{ row.a }}</td>
+          <td>{{ row.u }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+    <smart-pagination
+      :currentPage.sync="currentPage"
+      :totalPages="totalPages"
+      :maxPageLinks="5"
+    />
+    <br />
+    <p>
+      Qui si possono trovare
+      <strong>tutti i corsi di laurea {{ uni }}</strong
+      >. I corsi in questione sono relativi
+      <strong>all'anno accademico 2020/2021</strong>.
+    </p>
+    <p>
+      <strong> Come funziona?</strong> È molto semplice, basta digitare nella
+      barra di ricerca es. "data science" e comparirà la lista di tutti i corsi
+      con quel nome. Inoltre cliccando "Livello" vengono ordinati i corsi in
+      base al tipo di laurea.
+    </p>
+    <p>
+      <strong>Attenzione!</strong> La colonna test è per indicare quale corso ha
+      <strong>test d'ingresso </strong>o è a numero programmato. In quel caso il
+      corso sarà segnato come test sì, in caso di accesso libero come test no.
+    </p>
+    <br />
+  </div>
+</template>
+
+<script>
+//import corsi from "../corsi.json";
+import axios from "axios";
+
+export default {
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: "Corsi di laurea economia",
+    // all titles will be injected into this template
+    titleTemplate: "%s | corsiuniversitari.info",
+    meta: [
+      {
+        name: "description",
+        content:
+          "In questo sito potrai trovare rapidamente tutti i corsi di laurea di economia, tutti raggruppati su una comoda tabella.",
+      },
+      {
+        name: "keywords",
+        content: "Corsi di laurea, Corso di laurea, Corsi di laurea economia",
+      },
+    ],
+    link: [
+      {
+        rel: "canonical",
+        href: "https://www.corsiuniversitari.info/corsi-di-laurea-economia",
+      },
+   /*    {
+        rel: "preload",
+        href: "https://json-server-corsi.herokuapp.com/corsi?c=L-13&c=LM-6",
+        as: "fetch",
+      }, */
+    ],
+  },
+  data() {
+    return {
+      filters: {
+        n: { value: "", keys: ["n"] },
+      },
+      currentPage: 1,
+      totalPages: 0,
+      uni: "economia",
+      corsi: [],
+    };
+  },
+  async beforeCreate() {
+    try {
+      const res = await axios.get(
+        "https://json-server-corsi.herokuapp.com/corsi?c=L-16&c=L-18&L-33&LM-56&L-63&LM-77&LM-83&LM-82&L/GASTR&LM-49&LM-56&L-41&LM-47"
+      );
+
+      this.corsi = res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  /*  methods: {
+    FiltraLista: function () {
+      var array = [];
+      for (var x = 0; x < corsi.length; x++) {
+        if (corsi[x].c == "L-13" || corsi[x].c == "LM-6") {
+          array.push(corsi[x]);
+        }
+      }
+      return array;
+    },
+  }, */
+};
+</script>
