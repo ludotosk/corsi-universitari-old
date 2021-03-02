ea632ef5dacdaeb719c9daf5bc38e7bf433ffb26
ludotosk <toscanoludovico@gmail.com>
2021-03-02 14:56:36 +0100

    aggiunta icona bot

---
+++ src/area-accademica-uni.vue
@@ -0,0 +1,309 @@
+<template>
+  <div class="container-md" data-view>
+    <br />
+    <h1 class="text-secondary">Corsi di laurea divisi per area accademica e università</h1>
+    <div class="row">
+      <div class="col-md">
+        <br>
+        <h2 class="text-secondary">Area accademica</h2>
+        <hr />
+        <ul class="list-group">
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-biologia"
+              >Corsi di laurea biologia</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-biotecnologia"
+              >Corsi di laurea biotecnologia</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-dietistica"
+              >Corsi di laurea dietistica</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-farmacia"
+              >Corsi di laurea farmacia</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-filosofia"
+              >Corsi di laurea filosofia</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-fisica"
+              >Corsi di laurea fisica</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-fisioterapia"
+              >Corsi di laurea fisioterapia</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-infermieristica"
+              >Corsi di laurea infermieristica</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-informatica"
+              >Corsi di laurea informatica</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-logopedia"
+              >Corsi di laurea logopedia</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-matematica"
+              >Corsi di laurea matematica</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-medicina"
+              >Corsi di laurea medicina</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-psicologia"
+              >Corsi di laurea psicologia</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-scienze-della-formazione-primaria"
+              >Corsi di laurea scienze della formazione primaria</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-scienze-motorie"
+              >Corsi di laurea scienze motorie</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-storia"
+              >Corsi di laurea storia</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-giurisprudenza"
+              >Corsi di laurea giurisprudenza</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-lettere"
+              >Corsi di laurea lettere</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-economia"
+              >Corsi di laurea economia</router-link
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="col-md">
+        <br>
+        <h2 class="text-secondary">Ateneo</h2>
+        <hr />
+        <ul class="list-group">
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unibo"
+              >Corsi di laurea Unibo</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unipi"
+              >Corsi di laurea Unipi</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unipd"
+              >Corsi di laurea Unipd</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-sapienza"
+              >Corsi di laurea La Sapienza</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-bicocca"
+              >Corsi di laurea Bicocca</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-polimi"
+              >Corsi di laurea Polimi</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-uniba"
+              >Corsi di laurea Uniba</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unifi"
+              >Corsi di laurea Unifi</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unimi"
+              >Corsi di laurea Unimi</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unina"
+              >Corsi di laurea Unina</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unipa"
+              >Corsi di laurea Unipa</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unipd"
+              >Corsi di laurea Unipd</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unipr"
+              >Corsi di laurea Unipr</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-uniroma1"
+              >Corsi di laurea Uniroma1</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unisa"
+              >Corsi di laurea Unisa</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unito"
+              >Corsi di laurea Unito</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unikore"
+              >Corsi di laurea Unikore</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-univda"
+              >Corsi di laurea Univda</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-bocconi"
+              >Corsi di laurea Bocconi</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-ecampus"
+              >Corsi di laurea Ecampus</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-federico-ii"
+              >Corsi di laurea Federico II</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-mercatorum"
+              >Corsi di laurea Mercatorum</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-roma-tre"
+              >Corsi di laurea Roma Tre</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-tor-vergata"
+              >Corsi di laurea Tor Vergata</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unicusano"
+              >Corsi di laurea Unicusano</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link
+              class="nav-link active"
+              to="/corsi-di-laurea-uninettuno"
+              >Corsi di laurea Uninettuno</router-link
+            >
+          </li>
+          <li class="list-group-item">
+            <router-link class="nav-link active" to="/corsi-di-laurea-unipegaso"
+              >Corsi di laurea Unipegaso</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: "Area accademica",
+    titleTemplate: "%s | corsiuniversitari.info",
+    meta: [
+      {
+        name: "description",
+        content:
+          "Questa è la lista delle aree accademiche per le quali abbiamo filtrato tutti i corsi di laurea di tutte le università.",
+      },
+      {
+        name: "keywords",
+        content:
+          "Corsi di laurea, Corso di laurea, Corsi universitari, Orientamento universitario",
+      },
+    ],
+    link: [
+      {
+        rel: "canonical",
+        href: "https://www.corsiuniversitari.info/area-accademica",
+      },
+    ],
+  },
+};
+</script>
