9fe066151e393a3594afe0f7ccdb4e6875480fe9
ludotosk <toscanoludovico@gmail.com>
2021-06-11 16:40:40 +0200

    cambiato testo senza test

--- src/no-test/lista-corsi-di-laurea-senza-test.vue
+++ src/no-test/lista-corsi-di-laurea-senza-test.vue
@@ -6,7 +6,7 @@
     </h1>
     <br />
     <h2 class="has-text-grey is-size-2">
-      Quali sono i corsi di laurea 2021 senza test d'ingresso? [tutte le università]
+      Quali sono le università senza test d'ingresso? [corsi di laurea 2021]
     </h2>
     <hr />
     <p>
