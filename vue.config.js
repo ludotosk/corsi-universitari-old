810f284fc75c9040be5b0f7c33471151530a5caa
ludotosk <toscanoludovico@gmail.com>
2021-10-19 11:15:59 +0200

    aggiunta hidratation

--- vue.config.js
+++ vue.config.js
@@ -46,6 +46,13 @@ module.exports = {
       renderAfterTime: 2000,
       headless: true,
       maxConcurrentRoutes: 32,
+      postProcess: route => {
+        // Defer scripts and tell Vue it's been server rendered to trigger hydration
+        route.html = route.html
+          .replace(/<script (.*?)>/g, '<script $1 defer>')
+          .replace('id="app"', 'id="app" data-server-rendered="true"');
+        return route;
+      }
     }
   },
   devServer: {
