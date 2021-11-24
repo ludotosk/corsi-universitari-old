15a9c0241e056d5632a77f55cfbf2caaeb2b4bb0
ludotosk <toscanoludovico@gmail.com>
2021-11-24 10:20:37 +0100

    fixato tutto nuova pagina on

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -82,10 +82,10 @@
           <router-link to="/lauree-triennali-elenco" class="navbar-item"
             >Triennali</router-link
           >
-          <router-link
-            to="/lista-corsi-di-laurea-senza-test"
+          <a
+            href="/lista-corsi-di-laurea-senza-test"
             class="navbar-item"
-            >Corsi senza test</router-link
+            >Corsi senza test</a
           >
           <a href="https://www.corsiuniversitari.info/link" target="_blank" class="navbar-item"
             >Test medicina</a
